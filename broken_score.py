104ffdede17a2e2394117d4eebfb8333764e4aec
Dickson <194126F@mymail.nyp.edu.sg>
2022-07-27 11:36:10 +0800

    Changed broken_score program to use functions

--- broken_score.py
+++ broken_score.py
@@ -1,17 +1,25 @@
 """
 CP1404/CP5632 - Practical
-Broken program to determine score status
+Broken program to determine score status, with functions
 """
 
-# TODO: Fix this!
 
-score = float(input("Enter score: "))
+def main():
+    """Get numeric score and print its status"""
+    score = float(input("Enter score: "))
+    print(determine_score(score))
 
-if 0 > score > 100:
-    print("Invalid score")
-elif score >= 50:
-    print("Passable")
-elif score >= 90:
-    print("Excellent")
-else:
-    print("Bad")
+
+def determine_score(score):
+    """Determine the result of a score"""
+    if 0 > score > 100:
+        print("Invalid score")
+    elif score >= 50:
+        print("Passable")
+    elif score >= 90:
+        print("Excellent")
+    else:
+        print("Bad")
+
+
+main()
