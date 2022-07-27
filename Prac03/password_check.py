4a63024f18ae17338598cf637c87a0c831e61035
Dickson <194126F@mymail.nyp.edu.sg>
2022-07-27 12:52:59 +0800

    Implement password_check

---
+++ Prac03/password_check.py
@@ -0,0 +1,17 @@
+"""
+Get a password with minimum length and display asterisks
+"""
+
+MINIMUM_LENGTH = 5
+
+
+def main():
+    """Get a password with minimum length and print asterisks"""
+    password = input("Enter your password:")
+    while len(password) < MINIMUM_LENGTH:
+        print("Invalid. Password must be at least {} characters" .format(MINIMUM_LENGTH))
+        password = input("Enter your password: ")
+    print('*' * len(password))
+
+
+main()
