067ca4f96933644f4982449b52a4cd9f2b886993
Dickson <194126F@mymail.nyp.edu.sg>
2022-09-25 21:42:01 +0800

    Add comments for practical 10

--- Prac10/wiki.py
+++ Prac10/wiki.py
@@ -9,6 +9,7 @@ try:
     print(wikipedia.page(search))
 
 except wikipedia.DisambiguationError as e:
+    # get a random page
     random_search = random.choice(e.options)
     print(wikipedia.search(random_search))
     print(wikipedia.summary(random_search))
